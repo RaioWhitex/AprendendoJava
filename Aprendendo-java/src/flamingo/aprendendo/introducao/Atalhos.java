899e36fc8f649820ca7a53d6f4b8a0e1d9a4a52b
Gustavo Steferson <stefersongustavoprogramador@gmail.com>
2026-08-12 21:33:50 -0300

    Add files via upload

---
+++ Aprendendo-java/src/flamingo/aprendendo/introducao/Atalhos.java
@@ -0,0 +1,10 @@
+package flamingo.aprendendo.introducao;
+
+public class Atalhos {
+    //PSVMA = static void main(String[] args) {
+    //sout = System.out.println("atalhos");
+    static void main(String[] args) {
+        String nome = "Gustavo";
+        System.out.println("Meu nome é " + nome);
+    }
+}
