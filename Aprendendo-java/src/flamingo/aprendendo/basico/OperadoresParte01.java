dad42dfd84e8df94bb246c7b9f4eec26fcfbf6e3
Gustavo Steferson <stefersongustavoprogramador@gmail.com>
2026-08-13 21:09:19 -0300

    Add files via upload

---
+++ Aprendendo-java/src/flamingo/aprendendo/basico/OperadoresParte01.java
@@ -0,0 +1,30 @@
+package flamingo.aprendendo.basico;
+
+public class OperadoresParte01 {
+    static void main(String[] args) {
+        /*
+        * + Soma
+        * - Subtração
+        * / Divisão
+        * * Multiplicação
+        * % Resto da Divisão
+        * */
+
+        /*System.out.println(10 + 10);
+        System.out.println(10 - 10);
+        System.out.println(10 / 10);
+        System.out.println(10 * 10);
+        System.out.println(10 % 3);*/
+
+        /*int numero01 = 300;
+        int numero02 = 250;
+        int soma = numero01 + numero02;
+
+        System.out.println("A soma dos números = " + soma);*/
+
+        int primeironumero = 10;
+        int segundonumero = 20;
+
+        System.out.println((double) primeironumero / segundonumero);
+    }
+}
---
+++ Aprendendo-java/src/flamingo/aprendendo/introducao/Exercicio.java
@@ -0,0 +1,15 @@
+package flamingo.aprendendo.introducao;
+
+public class Exercicio {
+    /* Eu <nome> morando no endereço <endereço>, confirmo o sálario de <sálario> na data <data>*/
+    static void main(String[] args) {
+        //Definições
+        String nome = "Gustavo Steferson";
+        String endereço = "São Paulo, SP";
+        float salario = 1700.00f;
+        short dia = 05;
+        String mes = "Agosto";
+        //Respostas
+        System.out.println("Eu, " + nome + ", morando em: " + endereço + ". confirmo o sálario de: " + salario + " na data " + dia + " no mês de " + mes);
+    }
+}
