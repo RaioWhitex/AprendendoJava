27f7296f7f2ab993bb9ca4216374bb7fe1f29591
Gustavo Steferson <stefersongustavoprogramador@gmail.com>
2026-08-19 21:21:03 -0300

    Add files via upload

---
+++ Aprendendo-java/src/flamingo/aprendendo/basico/exercicios/estruturascondicionais/Exercicio02.java
@@ -0,0 +1,12 @@
+package flamingo.aprendendo.basico.exercicios.estruturascondicionais;
+
+public class Exercicio02 {
+    static void main(String[] args) {
+
+        short saldo = 1;
+
+        if (saldo >=1)
+
+        System.out.println();
+    }
+}
